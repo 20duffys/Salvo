67d57b3884641ed0a0e345e2938a8c21894e46e5
Sam Duffy <[Asduffy20@stanford.edu>
2016-06-30 21:04:53 -0400

    New connection

--- salvo.js
+++ salvo.js
@@ -1,5 +1,4 @@
 
-document.addEventListener('DOMContentLoaded', function() {
 var heading1 = document.createElement('h2')
 console.log(heading1)
 
@@ -8,11 +7,11 @@ for (var i = 1; i < 8; i++) {
    var tr = document.createElement('tr');
    for (var j = 1; j < 8; j++) {
        var td = document.createElement('td');
-       if (i%2 == j%2) {
-           td.className = "white";
-       } else {
-           td.className = "blue";
-       }
+      //  if (i%2 == j%2) {
+      //      td.className = "white";
+      //  } else {
+      //      td.className = "blue";
+      //  }
        tr.appendChild(td);
        td.id = 's' + i + j;
        table1.id = "A"
@@ -20,7 +19,7 @@ for (var i = 1; i < 8; i++) {
    table1.appendChild(tr);
 }
 document.body.appendChild(table1);
-});
+
 //
 // document.addEventListener('DOMContentLoaded', function() {
 //   var title1 = document.createElement('h1');
@@ -30,7 +29,7 @@ document.body.appendChild(table1);
 // };
 
 
-document.addEventListener('DOMContentLoaded', function() {
+
 var table2 = document.createElement("table");
 for (var i = 1; i < 8; i++) {
    var tr = document.createElement('tr');
@@ -48,27 +47,131 @@ for (var i = 1; i < 8; i++) {
    table2.appendChild(tr);
 }
 document.body.appendChild(table2);
-});
+
+//
+// document.addEventListener('DOMContentLoaded', function() {
+//   Randomly select pairs of squares and randomly add the classname hidden to that target square.
 
 
-var rowIndex = [];
-var colIndex = [];
 
-document.addEventListener('DOMContentLoaded', function() {
-  for (var q = 1; q < 8; q++) {
-    rowIndex.push("s1"+[q]);
-    var rowIndexkeys = document.createElement('p');
-  } for (var w = 1; w < 8; w++) {
-    colIndex.push("s" + [w] + "1");
-    var colIndexkeys = document.createElement('p');
-  }
-  console.log(rowIndex);
-  console.log(colIndex);
-  for (var r = 0; r < 7; r++) {
-    console.log(rowIndex[r]);
-    console.log(colIndex[r]);
-  }
-});
+switchColor();
+
+
+
+
+
+
+//   var nest1 = [];
+//   for (var b = 1; b < 8; b++) {
+//     for (var l = 1; l < 8; l++) {
+//        nest1.push(document.getElementById("s" + b + l ));
+//      }
+//    }
+//
+// });
+
+
+
+function switchColor() {
+
+    var targets1 = [];
+
+    for (var b = 1; b < 8; b++) {
+      for (var l = 1; l < 8; l++) {
+         targets1.push(document.getElementById("s" + b + l ));
+      }
+    }
+
+    for (var y = 0; y < targets1.length; y++ ) {
+      targets1[y].addEventListener('click', changeColor(targets1[y]));
+    }
+
+
+
+    var targets2 = [];
+
+    for (var b = 1; b < 8; b++) {
+      for (var l = 1; l < 8; l++) {
+         targets2.push(document.getElementById("q" + b + l ));
+      }
+    }
+
+    for (var y = 0; y < targets2.length; y++ ) {
+      targets2[y].addEventListener('click', changeColor(targets2[y]));
+    }
+
+
+    var RubberDucky = document.getElementById("s21");
+    RubberDucky.classList = "hidden";
+    console.log(RubberDucky);
+
+    function changeColor(i) {
+      console.log(i.classList.contains("hidden"));
+      if (i.classList.contains("hidden")) {
+      return function() {
+        i.style.backgroundColor= "#ff0000";
+      }
+    }
+    else {
+        return function() {
+        i.style.backgroundColor= "#669999";
+      }
+      }
+    }
+};
+
+
+
+//   for (var l = 1; l < 8; l++) {
+//     for (var b =1; b < 8; b++) {
+//       targets1.push(document.getElementById("s" + l + b ));
+//       var targetsP1 = document.getElementById("s" + l + b );
+//       console.log(targetsP1);
+//       targetsP1.addEventListener('click', ChangeColor(targetsP1))
+//     }
+//   }
+// }
+//
+//       }
+//     }
+//     for (var d = 0; d < targets1.length; d++) {
+//       targets1[d].addEventListener("click", function() {
+//         targets1[d].style.backgroundColor = "#666633";
+//       })
+//       function
+//     }
+//     console.log(targets1);
+//   }
+// //
+// // function ChangeColor(e) {
+// //   e.style.backgroundColor = "#ff0000";
+// // }
+
+
+
+
+// var rowIndex = [];
+// var colIndex = [];
+// var letters = ['a','b','c','d','e','f','g'];
+//
+// document.addEventListener('DOMContentLoaded', function() {
+//   for (var q = 2; q < 8; q++) {
+//     rowIndex.push("s1"+[q]);
+//     var rowIndexK = document.getElementById('s1' + q)
+//     for (var b = 0; b < 7; b++) {
+//       rowIndexK.innerHTMl = letters[b];
+//     }
+//   } for (var w = 2; w < 8; w++) {
+//     colIndex.push("s" + [w] + "1");
+//     var colIndexkeys = document.createElement('p');
+//   }
+//   console.log(rowIndex);
+//   console.log(colIndex);
+//   for (var r = 0; r < 7; r++) {
+//     console.log(rowIndex[r]);
+//     console.log(colIndex[r]);
+//   }
+// });
 
 
 
